fca2b41b153669dcbf9744317a3ccfa09bc3ee42
1 <chipiga2005@gmail.com>
2025-02-27 10:13:01 +0300

    4214221

--- Ychebka/ycheba.cpp
+++ Ychebka/ycheba.cpp
@@ -1,22 +1,24 @@
-/*//#1
+#include <stdio.h>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <stdlib.h>
+#include <time.h>
+const int N = 10;
 int main() {
     setlocale(LC_ALL, "Russian");
-    const int SIZE = 10;
-    int A[SIZE];
+    int A[N];
+    int* ptr = A; // указатель на первый элемент массива
+
     srand(time(0));
-    for (int ptr = 0; ptr < SIZE; ++ptr) {
-        *(A + ptr) = std::rand() % 101; //от 0 до 100
+    for (int i = 0; i < N; i++) {
+        *ptr = rand() % 100; // использование указателя
+        ptr++; // перемещение указателя к следующему элементу
     }
     std::cout << "сгенерированный массив: ";
-    for (int* ptr = A; ptr < A + SIZE; ++ptr) {
+    for (int* ptr = A; ptr < A + N; ++ptr) {
         std::cout << *ptr << " ";
     }
-    //поиск наибольшего элемента
     int* maxPtr = A; //указатель на 1й элемент
-    for (int ptr = 1; ptr < SIZE; ++ptr) {
+    for (int ptr = 1; ptr < N; ++ptr) {
         if (*(A + ptr) > *maxPtr) {
             maxPtr = A + ptr; //обновление указателя на максимальный элемент
         }
@@ -27,37 +29,37 @@ int main() {
         *A = temp;
     }
     std::cout << "преобразованный массив: ";
-    for (int ptr = 0; ptr < SIZE; ++ptr) {
+    for (int ptr = 0; ptr < N; ++ptr) {
         std::cout << *(A + ptr) << " ";
     }
     std::cout << std::endl;
     return 0;
-}*/
-//#2
+}
+/*//#2
 #include <iostream>
-#include <cstdlib> 
+#include <cstdlib>
 #include <ctime>   
 int main() {
     setlocale(LC_ALL, "Russian");
     const int size = 10; 
     double arr[size];    
-    double* ptr = arr;   //указатель 
-    double sum_negative = 0; // хранение суммы отр.чисел
+    double* ptr = arr;   //указатель на 1й элемент массива
+    double sum_negative = 0; 
     srand(time(0));
-    for (int i = 0; i < size; ++i) {
-        *(ptr + i) = 200.0 * rand() / RAND_MAX - 100.0; //диапазон [-100, 100]
+    for (double* p = ptr; p < ptr + size; ++p) {
+        *p = static_cast<double>(rand()) / RAND_MAX * 200 - 100; // диапазон [-100, 100]
     }
-    std::cout << "сгенерированный массив: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << *(ptr + i) << " ";
+    std::cout << "Сгенерированный массив: ";
+    for (double* p = ptr; p < ptr + size; ++p) {
+        std::cout << *p << " ";
     }
     std::cout << std::endl;
-    //сум отрицательных элементов
-    for (int i = 0; i < size; ++i) {
-        if (*(ptr + i) < 0) {
-            sum_negative += *(ptr + i);
+    // сумма отрицательных элементов
+    for (double* p = ptr; p < ptr + size; ++p) {
+        if (*p < 0) {
+            sum_negative += *p;
         }
     }
-    std::cout << "сумма отрицательных элементов: " << sum_negative << std::endl;
+    std::cout << "Сумма отрицательных элементов: " << sum_negative << std::endl;
     return 0;
-}
+}*/
